c93e1707262ea1239fedbe8ae13e2ddd06969027
sneha-khambal <snehakhambal0@gmail.com>
2025-06-14 18:07:48 +0530

    Remove ignored files from repo based on .gitignore

--- backend/config.js
+++
@@ -1,7 +0,0 @@
- export const PORT = 5000;
- export const MONGODB_LOCAL_URL = "mongodb://127.0.0.1:27017/Read&Repeat"
-export const MONGODB_REMOTE_URL ="mongodb+srv://snehakhambal11:2ATWZtw9n49uXsZg@cluster0.jr3lvdx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-
- export const CLOUDINARY_CLOUD_NAME= 'dt4ljluaq'         
-  export const CLOUDINARY_API_KEY= '435868736195399'              
-  export const CLOUDINARY_API_SECRET= 'M2P4uPyrYFPxDVj8t5b6fvqiP-M'   
